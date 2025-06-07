c4227552d6dba0a79c6ef3d42cec71e166b770ec
joshmainac <josh.mainac@protonmail.com>
2025-06-07 23:33:19 +0900

    add genko-editor page

---
+++ yomikae/src/app/genko-editor/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import GenkoTextEditor from '@/components/GenkoTextEditor'
+// import { useState } from 'react'
+// import { Editor } from '@tinymce/tinymce-react'
+
+export default function EditorPage() {
+    return (
+        <main className="min-h-screen p-6 bg-grey-100">
+            <h1 className="text-2xl font-bold mb-6">よみかえ</h1>
+            <div className="flex flex-col">
+                <div className="w-full">
+                    <GenkoTextEditor />
+                </div>
+            </div>
+        </main>
+    )
+}
